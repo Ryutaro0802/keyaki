e7776411bfc76b6b30efffcffacd8e4aebf990ec
Ryutaro <ne200152g@gmail.com>
2017-03-21 02:12:34 +0900

    commit

--- routes/index.js
+++ routes/index.js
@@ -4,38 +4,46 @@ const fs = require('fs');
 const client = require('cheerio-httpcli');
 const url = 'http://www.keyakizaka46.com/s/k46o/diary/member/list?ima=0000';
 const articles = [];
-const rss = `
-<?xml version=”1.0″ encoding=”utf-8″ ?>
-<rss version=”2.0″>
-<channel>
-  <title>欅坂ブログまとめ</title>
-  <link>（サイトのURL）</link>
-  <lastBuildDate>Sat, 28 Oct 2006 00:00:00 +0900</lastBuildDate>
-  <item>
-    <title>（各情報のタイトル）</title>
-    <link>（各情報のURL）</link>
-    <category>（カテゴリをつけたい場合は入力。「お知らせ」「更新情報」など）</category>
-    <pubDate>Sat, 28 Oct 2006 00:00:00 +0900</pubDate>
-  </item>
-</channel>
-</rss>
-`;
-
-
-fs.writeFile('public/rss.xml', rss);
+const articleItems = [];
+const date = new Date();
 
 client.fetch(url, {}, (err, $, res) => {
   $('article').each(function (i) {
-    articles[i] = { date: null, title: null, name: null };
+    articles[i] = { date: null, title: null, href: null, name: null, imgSrc: null, content: null };
 
     const date = articles[i].date = $(this).find('.box-date').eq(0).text() + $(this).find('.box-date').eq(1).text();
     const title = articles[i].title = $(this).find('.box-ttl h3').text();
+    const href = articles[i].href = $(this).find('.box-ttl a').attr('href');
     const name = articles[i].name = $(this).find('.box-ttl .name').text();
+    const imgSrc = articles[i].imgSrc = $(this).find('img').eq(0).attr('src');
+    const content = articles[i].content = $(this).find('.box-article').html();
+
+    articleItems.push(`<entry>
+        <id>id:${href}</id>
+		    <title>${title}</title>
+		    <link rel='alternate' type='text/html' href='${href}' />
+		    <updated>${date}</updated>
+        <content type="xhtml" xml:lang="ja" xml:base="http://diveintomark.org/">${content}</content>
+      </entry>`);
+  });
+
+  const atom = `<?xml version='1.0' encoding='UTF-8'?>
+<feed xmlns='http://www.w3.org/2005/Atom' xml:lang='ja'>
+	<id>tag:keyakizakafeed/</id>
+	<title>欅坂46ブログ更新情報</title>
+	<updated>${date}</updated>
+	<link rel='alternate' type='text/html' href='https://keyaki-blog.herokuapp.com/' />
+	<link rel='self' type='application/atom+xml' href='https://keyaki-blog.herokuapp.com/atom.xml' />
+</feed>`;
+
+  fs.writeFile('public/atom.xml', atom);
+
+  router.get('/', (req, res, next) => {
+    res.render('index', { articles });
   });
 });
 
-router.get('/', (req, res, next) => {
-  res.render('index', { articles });
-});
+
+
 
 module.exports = router;
