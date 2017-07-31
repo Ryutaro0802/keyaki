1b6b640b2310cade5212c3d296e661f183548713
Ryutaro Kawakami <ne200152g@gmail.com>
2017-07-31 09:35:16 +0900

    edit all.js

--- model/nogizaka/all.js
+++ model/nogizaka/all.js
@@ -3,26 +3,28 @@ const request = require('request');
 const feed = 'http://blog.nogizaka46.com/atom.xml';
 const req = request(feed);
 const feedparser = new FeedParser({});
-var items = [];
+const items = [];
 
 req.on('response', function (res) {
     this.pipe(feedparser);
 });
 
-feedparser.on('meta', function (meta) {
-    console.log('==== %s ====', meta.title);
-});
+// feedparser.on('meta', function (meta) {
+//     console.log('==== %s ====', meta.title);
+// });
 
 feedparser.on('readable', function () {
     while (item = this.read()) {
-        // console.log(item);
         items.push(item);
     }
 });
 
 feedparser.on('end', function () {
-    // show titles
     items.forEach(function (item) {
-        console.log('- [' + item.title + ']' + '(' + item.link + ')');
+        console.log(item.title, item.link, item.author);
     });
+
+    return items
+
+    module.exports.items = items;
 });
